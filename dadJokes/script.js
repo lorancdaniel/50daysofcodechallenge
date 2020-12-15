c77b8ea6d4500da26a1a156e69c3b05c4cb2fba3
lorancdan <lorancdan@gmail.com>
2020-12-15 07:09:35 +0100

    keyboard listener

--- dadJokes/script.js
+++ dadJokes/script.js
@@ -1,19 +1,6 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-// const generateJoke = () => {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   };
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke;
-//     });
-// };
-
 const generateJoke = async () => {
   const config = {
     headers: {
